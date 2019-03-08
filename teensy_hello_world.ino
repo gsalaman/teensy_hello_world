7859a48052129a9d87cb6db62f0d9667c28b3def
gsalaman <43499190+gsalaman@users.noreply.github.com>
2019-03-08 05:12:59 -0700

    sizeof test

--- teensy_hello_world.ino
+++ teensy_hello_world.ino
@@ -10,16 +10,22 @@ void setup()
 
   digitalWrite(LED_PIN, pin_state);
 
+  // Note:  this won't actually print, as USB connects *after* the teensy is running.
   Serial.println("Hello world");
 }
 
 void loop() 
 {
+   int myInt;
+   
    if (Serial.available())
    {
      char c=Serial.read();
      Serial.println("Toggle LED");
      pin_state = !pin_state;
      digitalWrite(LED_PIN, !pin_state);
+
+     Serial.print("Size of int: ");
+     Serial.println(sizeof(myInt));
    }
 }
